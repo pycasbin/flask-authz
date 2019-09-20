ca3e0076e6f67a831dc28c187bc4bb87603c9b9a
Jesse Cooper <jesse.cooper@sciencelogic.com>
2019-09-20 11:57:22 -0500

    init unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+import flask
+import pytest
+
+@pytest.fixture
+def app_fixture():
+    app = flask.Flask('test')
+    app.testing = True
+    app.config['CASBIN_MODEL'] = os.path.split(os.path.realpath(__file__))[
+                                     0] + '/casbin_files/rbac_model.conf'
+    # Set headers where owner for enforcement policy should be located
+    app.config['CASBIN_OWNER_HEADERS'] = {'X-User', 'X-Idp-Groups'}
+
+    yield app
