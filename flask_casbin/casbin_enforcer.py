1c667c8cf532beb5833a8620fb7f5abe1d5cde2b
Jesse Cooper <jesse.cooper@sciencelogic.com>
2019-09-20 13:21:28 -0500

    blacked package

--- flask_casbin/casbin_enforcer.py
+++ flask_casbin/casbin_enforcer.py
@@ -9,6 +9,7 @@ class CasbinEnforcer:
     """
     Casbin Enforce decorator
     """
+
     e = None
 
     def __init__(self, app, adapter):
@@ -19,48 +20,37 @@ class CasbinEnforcer:
         """
         self.app = app
         self.adapter = adapter
-        self.e = casbin.Enforcer(
-            app.config.get('CASBIN_MODEL'),
-            self.adapter,
-            True
-        )
+        self.e = casbin.Enforcer(app.config.get("CASBIN_MODEL"), self.adapter, True)
 
     def enforcer(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             # Check sub, obj act against Casbin polices
             self.app.logger.debug(
-                "Enforce Headers Config: %s\nRequest Headers: %s" % (
-                    self.app.config.get('CASBIN_OWNER_HEADERS'),
-                    request.headers
-                )
+                "Enforce Headers Config: %s\nRequest Headers: %s"
+                % (self.app.config.get("CASBIN_OWNER_HEADERS"), request.headers)
             )
-            for header in self.app.config.get('CASBIN_OWNER_HEADERS'):
+            for header in self.app.config.get("CASBIN_OWNER_HEADERS"):
                 if request.headers.has_key(header):
                     for owner in request.headers.getlist(header):
                         self.app.logger.debug(
-                            "Enforce against owner: %s header: %s" % (
-                                owner.strip('"'),
-                                header
-                            )
+                            "Enforce against owner: %s header: %s"
+                            % (owner.strip('"'), header)
                         )
                         if self.e.enforce(
-                            owner.strip('"'),
-                            str(request.url_rule),
-                            request.method
+                            owner.strip('"'), str(request.url_rule), request.method
                         ):
                             return func(*args, **kwargs)
             else:
-                return (
-                    jsonify({'message': 'Unauthorized'}), 401)
+                return (jsonify({"message": "Unauthorized"}), 401)
 
         return wrapper
 
     def manager(self, func):
         """Get the Casbin Enforcer Object to manager Casbin"""
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             return func(self.e, *args, **kwargs)
 
         return wrapper
-
--- tests/test_casbin_enforcer.py
+++ tests/test_casbin_enforcer.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask_casbin import CasbinEnforcer
 
+
 @pytest.fixture
 def enforcer(app_fixture):
     engine = create_engine("sqlite://")
@@ -17,67 +18,64 @@ def enforcer(app_fixture):
     Base.metadata.create_all(engine)
     s = session()
     s.query(CasbinRule).delete()
-    s.add(CasbinRule(ptype='p', v0='alice', v1='/item', v2='GET'))
-    s.add(CasbinRule(ptype='p', v0='bob', v1='/item', v2='GET'))
-    s.add(CasbinRule(ptype='p', v0='data2_admin', v1='/item', v2='POST'))
-    s.add(CasbinRule(ptype='p', v0='data2_admin', v1='/item', v2='DELETE'))
-    s.add(CasbinRule(ptype='p', v0='data2_admin', v1='/item', v2='GET'))
-    s.add(CasbinRule(ptype='g', v0='alice', v1='data2_admin'))
-    s.add(CasbinRule(ptype='g', v0='users', v1='data2_admin'))
+    s.add(CasbinRule(ptype="p", v0="alice", v1="/item", v2="GET"))
+    s.add(CasbinRule(ptype="p", v0="bob", v1="/item", v2="GET"))
+    s.add(CasbinRule(ptype="p", v0="data2_admin", v1="/item", v2="POST"))
+    s.add(CasbinRule(ptype="p", v0="data2_admin", v1="/item", v2="DELETE"))
+    s.add(CasbinRule(ptype="p", v0="data2_admin", v1="/item", v2="GET"))
+    s.add(CasbinRule(ptype="g", v0="alice", v1="data2_admin"))
+    s.add(CasbinRule(ptype="g", v0="users", v1="data2_admin"))
     s.commit()
     s.close()
 
-    yield CasbinEnforcer(
-        app_fixture,
-        adapter
-    )
+    yield CasbinEnforcer(app_fixture, adapter)
+
 
 @pytest.mark.parametrize(
-    'header, user, method, status',
+    "header, user, method, status",
     [
-        ('X-User', 'alice', 'GET', 200),
-        ('X-User', 'alice', 'POST', 201),
-        ('X-User', 'alice', 'DELETE', 202),
-        ('X-User', 'bob', 'GET', 200),
-        ('X-User', 'bob', 'POST', 401),
-        ('X-User', 'bob', 'DELETE', 401),
-        ('X-Idp-Groups', 'admin', 'GET', 401),
-        ('X-Idp-Groups', "users", 'GET', 200),
-    ]
+        ("X-User", "alice", "GET", 200),
+        ("X-User", "alice", "POST", 201),
+        ("X-User", "alice", "DELETE", 202),
+        ("X-User", "bob", "GET", 200),
+        ("X-User", "bob", "POST", 401),
+        ("X-User", "bob", "DELETE", 401),
+        ("X-Idp-Groups", "admin", "GET", 401),
+        ("X-Idp-Groups", "users", "GET", 200),
+    ],
 )
 def test_enforcer(app_fixture, enforcer, header, user, method, status):
-
-    @app_fixture.route('/')
+    @app_fixture.route("/")
     @enforcer.enforcer
     def index():
         return jsonify({"message": "passed"}), 200
 
-    @app_fixture.route('/item', methods=['GET', 'POST', 'DELETE'])
+    @app_fixture.route("/item", methods=["GET", "POST", "DELETE"])
     @enforcer.enforcer
     def item():
-        if request.method == 'GET':
+        if request.method == "GET":
             return jsonify({"message": "passed"}), 200
-        elif request.method == 'POST':
+        elif request.method == "POST":
             return jsonify({"message": "passed"}), 201
-        elif request.method == 'DELETE':
+        elif request.method == "DELETE":
             return jsonify({"message": "passed"}), 202
 
     headers = {header: user}
     c = app_fixture.test_client()
-    #c.post('/add', data=dict(title='2nd Item', text='The text'))
-    rv = c.get('/')
+    # c.post('/add', data=dict(title='2nd Item', text='The text'))
+    rv = c.get("/")
     assert rv.status_code == 401
     caller = getattr(c, method.lower())
-    rv = caller('/item', headers=headers)
+    rv = caller("/item", headers=headers)
     assert rv.status_code == status
 
 
 def test_manager(app_fixture, enforcer):
-    @app_fixture.route('/manager', methods=['POST'])
+    @app_fixture.route("/manager", methods=["POST"])
     @enforcer.manager
     def manager(manager):
         assert isinstance(manager, Enforcer)
         return jsonify({"message": "passed"}), 201
 
     c = app_fixture.test_client()
-    c.post('/manager')
+    c.post("/manager")
