58614d813219b25c791da5712d49f9ead51a6526
Douglas Rohde <drohde@sciencelogic.com>
2019-09-24 15:43:32 -0400

    author and author email updates

--- setup.py
+++ setup.py
@@ -2,39 +2,41 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+dependency_links = [
+    x.strip().replace("git+", "") for x in all_reqs if x.startswith("git+")
+]
 
 setup(
-    name='flask-casbin',
+    name="flask-casbin",
     version=__version__,
-    description='flask-casbin',
+    description="flask-casbin",
     long_description=long_description,
-    url='https://github.com/sciencelogic/flask-casbin',
-    download_url='https://github.com/sciencelogic/flask-casbin/tarball/' + __version__,
-    license='MIT',
+    url="https://github.com/sciencelogic/flask-casbin",
+    download_url="https://github.com/sciencelogic/flask-casbin/tarball/" + __version__,
+    license="MIT",
     classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python :: 3',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
     ],
-    keywords=['pycasbin', 'casbin', 'flask', 'rbac'],
-    packages=find_packages(exclude=['docs', 'tests*']),
+    keywords=["pycasbin", "casbin", "flask", "rbac"],
+    packages=find_packages(exclude=["docs", "tests*"]),
     include_package_data=True,
-    author='Jesse Cooper',
+    author="ScienceLogic",
     install_requires=install_requires,
     dependency_links=dependency_links,
-    author_email='jesse.cooper@sciencelogic.com'
+    author_email="airlocks@sciencelogic.com",
 )
