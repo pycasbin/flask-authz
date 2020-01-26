fb315f02aefffeb5955cf60b873e28d02e86e045
Yang Luo <hsluoyz@qq.com>
2020-01-26 21:02:44 +0800

    Bump to v1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(desc_file, "r") as fh:
 
 setuptools.setup(
     name="flask-authz",
-    version="0.0.1",
+    version="1.0.0",
     author="Yang Luo",
     author_email="hsluoyz@gmail.com",
     description="An authorization middleware for Flask that supports ACL, RBAC, ABAC, based on Casbin",
@@ -17,14 +17,13 @@ setuptools.setup(
     keywords=["flask", "casbin", "auth", "authz", "acl", "rbac", "abac", "access control", "authorization", "permission"],
     packages=setuptools.find_packages(),
     install_requires=['casbin', 'flask', 'werkzeug'],
-    python_requires=">=3.3",
+    python_requires=">=3.5",
     license="Apache 2.0",
     classifiers=[
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
